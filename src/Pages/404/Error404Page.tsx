99b1c83513597be3ee361cab08701bf04be0878c
Bogdans Zabela <bogdanz@inbox.lv>
2022-05-12 18:15:29 +0300

    update

--- src/Pages/404/Error404Page.tsx
+++ src/Pages/404/Error404Page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Error404Page = () => (
-  <div />
+  <div className="container">
+    <div className="row">
+      <div className="col-md-12 center-md">
+        <h1>404 ERROR</h1>
+      </div>
+    </div>
+  </div>
 );
 
 export default Error404Page;
